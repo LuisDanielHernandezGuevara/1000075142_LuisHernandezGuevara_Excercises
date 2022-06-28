4862b83a77cb5f4757beae08143847208ff50ff0
1000075142 <1000075142@Maverick-VM-5.corp.hexaware.com>
2022-06-28 13:37:34 -0500

    75142_Added deleted file

---
+++ src/main/java/Delete.java
@@ -0,0 +1,45 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Delete {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String urlDb = "jdbc:mysql://localhost:3306/students_75412?useSSL=false";
+		String user = "students";
+		String password = "Password123";
+		
+		
+		Connection cn = null;
+		Statement myStatement =null;
+		ResultSet myResult = null;
+		
+		try {
+			cn =DriverManager.getConnection(urlDb, user , password);
+			myStatement = cn.createStatement();
+			
+			int affectedrows= myStatement.executeUpdate(
+					"delete from DATASHEET " +
+							"where NAME= 'Daniel' "
+					);
+			
+			myResult=myStatement.executeQuery("SELECT * FROM DATASHEET");
+			
+		while(myResult.next()) {
+			System.out.println(myResult.getString("NAME"));
+			System.out.println(myResult.getString("LASTNAME"));
+			System.out.println(myResult.getString("EMAIL"));
+
+		}
+			
+			
+		}catch(Exception ex) {
+			System.out.print(ex);
+		}
+
+	}
+
+}

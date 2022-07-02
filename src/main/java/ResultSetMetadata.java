29f1b02edb277bbe4f801a5a321fc44989409ddf
1000075142 <1000075142@Maverick-VM-01.corp.hexaware.com>
2022-07-01 19:45:05 -0500

    75142_ResultSetGetMetaData

--- src/main/java/ResultSetMetadata.java
+++ src/main/java/ResultSetMetadata.java
@@ -1,9 +1,39 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class ResultSetMetadata {
+import com.mysql.cj.jdbc.result.ResultSetMetaData;
+import com.mysql.cj.xdevapi.Statement;
 
-	public static void main(String[] args) {
+public class ResultSetMetadata  {
+
+	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
-
+		Connection myConn = null;
+		Statement myStmnt = null;
+		ResultSet rs = null;
+		
+		
+		try {
+			//Get a connection to a DB 
+			myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/students?75142","students","Password123");
+			
+			//Run query
+			myStmnt = (Statement) myConn.createStatement();
+			rs = ((java.sql.Statement) myStmnt).executeQuery("select NAME,LASTNAME from DATASHEETS ");
+			
+			ResultSetMetaData rsMetaData = (ResultSetMetaData) rs.getMetaData();
+			
+			int columnCount = rsMetaData.getColumnCount();
+			System.out.print("Column count :" + columnCount + "\n");
+			
+			
+		}catch() {
+			
+		}
+		
+		
 	}
 
 }
